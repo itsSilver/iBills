20135dffe433242cbabe5bd0491edbc71b5c9881
SilverTurku21 <sturku@helius-software.com>
2023-03-17 12:59:13 +0100

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Internal Blockchain',
+    title: 'Explorer Blockchain',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
